97094e1f27f4caf4e16fd231e895614c2d59ed6c
Andriy Rukas <rukasav@gmail.com>
2024-02-22 12:28:59 +0200

    Props and how to use them

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,6 +1,8 @@
 import Button from "./Button/Button.jsx"
 import Button2 from "./Button2.jsx"
 import Card from "./Card/Card.jsx"
+import Student from "./Student.jsx";
+
 
 
 function App() {
@@ -9,7 +11,11 @@ function App() {
       <Card />
       <Button />
       <Button2 />
-
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Patric" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student name="Larry"/>
     </>
   );
 }
